f1628d1d1b998d8dbab4e82a11eed39d8f0966e9
kecajjo <kecajorigami@gmail.com>
2020-01-24 16:20:48 +0100

    .

--- send_orders.py
+++ send_orders.py
@@ -20,26 +20,20 @@ def start_sending_orders(orders):
                 target, value2, value3 = orders.get()
                 if target == 6:
                     gimbal.move_up()
-                    print("gimbal moving up")
                 if target == 7:
                     gimbal.move_down()
-                    print("gimbal moving down")
             except:
                 print("smthin fcked up", target)
             if (target > 1 and target < 6) or target == 10:
                 try:
                     if target == 2:
                         motor.turn_right(pwm1,pwm2)
-                        print("turning right")
                     if target == 3:
                         motor.turn_left(pwm1,pwm2)
-                        print("turning left")
                     if target == 4:
                         motor.move_forth(pwm1,pwm2)
-                        print("moving forward")
                     if target == 5:
                         motor.move_back(pwm1,pwm2)
-                        print("moving back")
                     if target == 10:
                         motor.stop()
                 except:
